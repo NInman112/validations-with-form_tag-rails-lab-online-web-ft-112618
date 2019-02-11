84477167fd59f64e5d55e0785bfbc9d719fb2a4a
Nick Inman <nick@theoatmeal.com>
2019-02-11 23:42:02 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, uniqueness: true 
+  validates :email, uniqueness: true
+  validate :phone_number, length: {10}
 end
